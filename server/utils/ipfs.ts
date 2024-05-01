0754a84a7c62f97caca6c7cfee002f1d24f048b7
angelorc <angelo@rcstart.it>
2024-05-01 11:49:12 +0200

    chore: add transcoding log

--- server/utils/ipfs.ts
+++ server/utils/ipfs.ts
@@ -2,6 +2,7 @@ export const useIpfsLink = (cid: string | undefined): string => {
   if (!cid) return ''
 
   const url = useRuntimeConfig().ipfs.gateway
+  console.log('IPFS URL:', url)
   if (cid.startsWith('ipfs://')) {
     const newCid = cid.replace('ipfs://', '')
     return url.replace('{cid}', newCid)
