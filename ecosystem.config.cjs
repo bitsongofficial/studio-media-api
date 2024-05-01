3ab21ea464e5d718a647db441712cb39efa7fa39
angelorc <angelo@rcstart.it>
2024-05-01 12:03:53 +0200

    update pm2 default port

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   apps: [
     {
       name: "studio-media-api",
-      port: "3000",
+      port: "3001",
       exec_mode: "cluster",
       instances: "max",
       script: "./.output/server/index.mjs",
