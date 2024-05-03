57452655e42714ccd0e0a19f43d7cb9dcc672497
angelorc <angelo@rcstart.it>
2024-05-03 12:52:00 +0200

    fix cached storage

--- server/utils/cache.ts
+++ server/utils/cache.ts
@@ -1,20 +1,13 @@
 import prisma from './db'
-// export const cachedStorageData = defineCachedFunction(async (cid: string) => {
-//   return await prisma.storage_ipfs.findUnique({
-//     where: {
-//       id: cid
-//     }
-//   })
-// }, {
-//   maxAge: 7 * 24 * 60 * 60, // 7 days
-//   name: 'storage_ipfs',
-//   getKey: (cid: string) => cid,
-// })
 
-export const cachedStorageData = async (cid: string) => {
+export const cachedStorageData = defineCachedFunction(async (cid: string) => {
   return await prisma.storage_ipfs.findUnique({
     where: {
       id: cid
     }
   })
-}
+}, {
+  maxAge: 7 * 24 * 60 * 60, // 7 days
+  name: 'storage_ipfs',
+  getKey: (cid: string) => cid,
+})
