7c960f726c22da918602929aa17599aad9c3842f
angelorc <angelo@rcstart.it>
2024-05-03 13:13:33 +0200

    fix pm2 setup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,12 +5,15 @@ module.exports = {
   apps: [
     {
       name: "studio-media-api",
-      port: "3001",
-      exec_mode: "cluster",
+      port: process.env.PORT || 3000,
+      // Do not use cluster mode, it will not work with the current setup
+      // exec_mode: "cluster",
+      // Use only one instance
       instances: "1",
       script: "./.output/server/index.mjs",
       env: {
         NODE_ENV: "production",
+        PORT: process.env.PORT || 3000,
         ...process.env,
       },
     },
