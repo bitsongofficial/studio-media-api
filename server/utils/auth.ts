082d24831dfafeaeed4c0fbf5a6860b3b61c3f8a
angelorc <angelo@rcstart.it>
2024-07-22 12:55:49 +0200

    replace admin

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -10,7 +10,7 @@ export async function ensureAuth(event: H3Event) {
     'bitsong1h882ezq7dyewld6gfv2e06qymvjxnu842586h2', //dev
     'bitsong1q49ntyz2wjurrm56ymm5hjnz60ya3x783c5m6e',
     'bitsong1s5kwp97f2fsysy2rfwv4w5kl06rnta8fj06e2y', //dev2
-    'bitsong1yyjyrzuk6r3fcrjrxyumgy85ys8lr9kn6xzxcj'
+    'bitsong144n93v67ld0x49v6nceqyw44gzmc6du0r30dve'
   ]
 
   if (user === null) {
