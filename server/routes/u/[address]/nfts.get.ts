e4441c78da8e456c5a35f1b60cc85facbeca0b38
angelorc <angelo@rcstart.it>
2024-07-23 12:55:00 +0200

    chore: optimize nfts.get.ts code for better performance

--- server/routes/u/[address]/nfts.get.ts
+++ server/routes/u/[address]/nfts.get.ts
@@ -1,27 +1,43 @@
 import { getNftsByOwner } from "~/utils/bitsong"
+import { z } from 'zod'
+
+const schema = z.object({
+  includeValue: z.boolean().optional().default(true)
+})
 
 export default defineEventHandler(async (event) => {
   const address: string = getRouterParam(event, 'address')
   if (!address) throw createError({ statusCode: 400, statusMessage: 'Bad Request' })
 
+  const { includeValue } = await getValidatedQuery(event, schema.parse)
+
   const data = await getNftsByOwner(address)
 
-  const nfts = await Promise.all(data.nfts.map(async (nft) => {
-    const config = await $fetch(`/nfts/${nft.nft}/config`)
-
-    const { sellPrice } = useCurveSimulator(nft.totalIds, {
-      ratio: config.ratio,
-      sellerFeeBps: config.sellerFeeBps,
-      referralFeeBps: config.referralFeeBps,
-      protocolFeeBps: config.protocolFeeBps,
-      totalSupply: config.totalSupply,
-    })
-
+  let nfts = data.nfts.map((nft) => {
     return {
       ...nft,
-      value: sellPrice.totalPrice,
+      value: 0
     }
-  }))
+  })
+
+  if (includeValue) {
+    nfts = await Promise.all(data.nfts.map(async (nft) => {
+      const config = await $fetch(`/nfts/${nft.nft}/config`)
+
+      const { sellPrice } = useCurveSimulator(nft.totalIds, {
+        ratio: config.ratio,
+        sellerFeeBps: config.sellerFeeBps,
+        referralFeeBps: config.referralFeeBps,
+        protocolFeeBps: config.protocolFeeBps,
+        totalSupply: config.totalSupply,
+      })
+
+      return {
+        ...nft,
+        value: sellPrice.totalPrice,
+      }
+    }))
+  }
 
   return {
     totalCount: data.totalCount,
